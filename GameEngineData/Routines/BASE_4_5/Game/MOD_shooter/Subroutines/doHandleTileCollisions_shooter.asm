75064bcf90bd17765866281f4e2a20898ce51f9a
Kevin81 <kevin.van.der.burg@gmail.com>
2026-02-24 00:30:01 +0100

    Refactoring of shooter module scripts

--- GameEngineData/Routines/BASE_4_5/Game/MOD_shooter/Subroutines/doHandleTileCollisions_shooter.asm
+++ GameEngineData/Routines/BASE_4_5/Game/MOD_shooter/Subroutines/doHandleTileCollisions_shooter.asm
@@ -1,177 +1,96 @@
-    
-    ;LDA collisionsToCheck ;; declared in doHandlePhysics script
-                            ;; the last nibble, each bit corresponding to a point to check.
-                            ;; 0001 = top left
-                            ;; 0010 = top right
-                            ;; 0100 = bottom right
-                            ;; 1000 = bottom left
-                            
-    ;;; will only need to use col pt 0 and 1 to see if we should evaluate from col table 1 or 2.
-    ;;; since they represent the left and the right of the object, the other checks would be
-    ;;; redundant.
-    LDA Object_flags,x
-    AND #%00000110
-    BNE +doColCheck
-        JMP doneWithTileCollion
-+doColCheck
 
+MACRO CheckCollisionAtTileXY
+    JSR GetTileAtPosition
+    LDA tempx
+    BEQ +evenCollisionTable
+
+    +oddCollisionTable:
+    LDA collisionTable2,y
+    JMP +checkPoint
+
+    +evenCollisionTable:
+    LDA collisionTable,y
+
+    +checkPoint:
+    BEQ +checkNextPoint
+        LDA tempx
+        STA temp2
+        STY tempy
+        LDA #$01
+        RTS
+    +checkNextPoint:
+ENDM
+
+
+    ;; Reset the end result variable
+    LDA #$00
+    STA tempA
+    STA tempy
+
+    ;; Check if collision check is needed (based on object flags 1 and 2)
+	LDA Object_flags,x
+	AND #%00000110
+	BNE +doColCheck
+		RTS
+    +doColCheck
+
+    ;; Store if current screen is odd or even in a temp variable
     LDA xHold_screen
     AND #%00000001
-    STA tempx ;; we will flip this if we have crossed threshold for left points
+    STA tempx
 
+    ;; Check point 1 (the top right of the object)
     LDA yHold_hi
     CLC
     ADC self_top
     STA tileY
-    
-    LDA xHold_hi
-    CLC
-    ADC self_left
-    STA tileX
-    BCC +pointDidNotCrossThreshold
-        ;; x point did cross threshold.
-    LDA tempx
-    EOR #%00000001
-    STA tempx
-+pointDidNotCrossThreshold
-    
-    JSR GetTileAtPosition
-    LDA tempx
-    AND #%00000001
-    BEQ inLeftCt
-        LDA #$01
-        STA temp2
-        LDA collisionTable2,y
-        JMP gotPoint0
-    inLeftCt:
-        LDA #$00
-        STA temp2
-        LDA collisionTable,y
-gotPoint0:
-    STA tempA
-    BEQ noCollision_point0
-    JMP doneWithTileCollion
-noCollision_point0:
 
-
-    LDA xHold_screen
-    AND #%00000001
-    STA tempy ;; for right points. 
-    ; LDA yHold_hi
-    ; CLC
-    ; ADC self_top
-    ; STA tileY
-    
-    
     LDA xHold_hi
     CLC
     ADC self_right
     STA tileX
-    BCC +pointDidNotCrossThreshold
-        LDA tempy
+    BCC +getTileOffset
+        ;; We've crossed a page boundary; flip the screen number
+        LDA tempx
         EOR #%00000001
-        STA tempy
-+pointDidNotCrossThreshold:
-    
+        STA tempx
+    +getTileOffset:
+    CheckCollisionAtTileXY
 
-    LDA tempy
-    BEQ inLeftCt2
-inRightCt2:
-        LDA #$01
-        STA temp2
-    JSR GetTileAtPosition
-    LDA collisionTable2,y
-    JMP gotPoint1
-inLeftCt2
-        LDA #$00
-        STA temp2
-    JSR GetTileAtPosition
-    LDA collisionTable,y
-    JMP gotPoint1
-    
-gotPoint1:
-    STA tempB
-    BEQ noCollision_point1
-    JMP doneWithTileCollion
-
-noCollision_point1:
-
-
-        
+    ;; Check point 2 (the bottom right of the object)
     LDA yHold_hi
     CLC
     ADC self_bottom
     STA tileY
+    CheckCollisionAtTileXY
 
-    LDA tempy
+    ;; Reset xHold_screen for left boundary check
+    LDA xHold_screen
     AND #%00000001
-    BEQ inLeftCt3
-inRightCt3:
-        LDA #$01
-        STA temp2
-    JSR GetTileAtPosition
-    LDA collisionTable2,y
-    JMP gotPoint2
-inLeftCt3
-        LDA #$00
-        STA temp2
-    JSR GetTileAtPosition
-    LDA collisionTable,y
-    JMP gotPoint2
-    
-gotPoint2:
-    STA tempC
-    BEQ noCollision_point2
-    JMP doneWithTileCollion
+    STA tempx
 
-    
-    
-noCollision_point2:    
-
-
-
-    
-        
-    LDA yHold_hi
-    CLC
-    ADC self_bottom
-    STA tileY
-    
-    
+    ;; Check point 3 (the bottom left of the object)
     LDA xHold_hi
     CLC
     ADC self_left
     STA tileX
-    
+    BCC +getTileOffset
+        ;; We've crossed a page boundary; flip the screen number
+        LDA tempx
+        EOR #%00000001
+        STA tempx
+    +getTileOffset:
+    CheckCollisionAtTileXY
 
-    LDA tempx
-    AND #%00000001
-    BEQ inLeftCt4
-inRightCt4:
-        LDA #$01
-        STA temp2
-    JSR GetTileAtPosition
-    LDA collisionTable2,y
-    JMP gotPoint3
-inLeftCt4
-        LDA #$00
-        STA temp2
-    JSR GetTileAtPosition
-    LDA collisionTable,y
-    JMP gotPoint3
-    
-gotPoint3:
-    STA tempD
-    BEQ noCollision_point3
-    JMP doneWithTileCollion
+    ;; Check point 4 (the top left of the object)
+    LDA yHold_hi
+    CLC
+    ADC self_top
+    STA tileY
+    CheckCollisionAtTileXY
 
+    ;; All checked: load tile offset and type and return
+    LDY tempy
+    LDA tempA
+    ;RTS
 
-noCollision_point3:
-
-doneWithTileCollion:
-    ;;; now the first non-zero collision point that this object encountered
-    ;;; is in the accumulator.
-    ;;; if the result was zero, doHandleObjects will skip tile behaviors.
-    ;;;; otherwise, it will run a trampoline to that behavior.
-    ;;;; this will allow us to utilize other banks and whatnot, since doHandleObjects
-    ;;;; is in the static bank.
--- GameEngineData/Routines/BASE_4_5/Game/MOD_shooter/Tiles/hurtPlayerTile_shooter.asm
+++ GameEngineData/Routines/BASE_4_5/Game/MOD_shooter/Tiles/hurtPlayerTile_shooter.asm
@@ -1,12 +1,13 @@
 
-    ;;;;;;;;;;;;;;;;;; Presumes there is a variable called myLives defined in user variables.
-    ;;;;;;;;;;;;;;;;;; You could also create your own variable for this.
+    ;; Presumes there is a variable called myLives defined in user variables.
+    ;; You could also create your own variable for this.
 
     LDA gameHandler
     AND #%10000000
     BEQ +canHurtPlayer
         JMP +skipHurt
-+canHurtPlayer:
+    +canHurtPlayer:
+
     CPX player1_object
     BEQ +doPlayer
         LDA Object_flags,x
@@ -14,18 +15,19 @@
         BNE +notWeapon
             DestroyObject
         +notWeapon
+
         JMP +done
-    +doPlayer
+    +doPlayer:
+
     DEC myLives
     LDA myLives
     BNE +notGameOver
         ;; is game over.
         JMP RESET
-    +notGameOver
+    +notGameOver:
     
     LDA continueMap
     STA warpMap
-    
     LDA continueX
     STA newX
     LDA continueY
@@ -34,13 +36,8 @@
     LDA continueScreen
     STA warpToScreen
     STA camScreen
-    
-    
-    
-    WarpToScreen warpToMap, warpToScreen, #$02
-        ;; arg0 = warp to map.  0= map1.  1= map2.
-        ;; arg1 = screen to warp to.
-        ;; arg2 = screen transition type - most likely use 1 here.
-            ;; 1 = warp, where it observes the warp in position for the player.
 
-+done
+    WarpToScreen warpToMap, warpToScreen, #$02
+    
+    +done:
+
