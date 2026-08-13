41cd5dd604e807c33d0c315b3970d30217c847a5
Kevin van der Burg <kevin@toscani.nl>
2026-08-13 16:07:46 +0200

    Moved CheckCollisionAtTileXY to macros
    (to add some consistency to script usage)

--- GameEngineData/Routines/BASE_4_5/Game/MOD_PlatformerBase/Subroutines/doHandleTileCollisions_PlatformBase.asm
+++ GameEngineData/Routines/BASE_4_5/Game/MOD_PlatformerBase/Subroutines/doHandleTileCollisions_PlatformBase.asm
@@ -1,40 +1,4 @@
 
-MACRO CheckCollisionAtTileXY
-    JSR GetTileAtPosition
-    LDA tempx
-    BEQ +evenCollisionTable
-
-    +oddCollisionTable:
-    LDA collisionTable2,y
-    JMP +checkPoint
-
-    +evenCollisionTable:
-    LDA collisionTable,y
-
-    +checkPoint:
-    BEQ +checkNextPoint
-    CMP #$01
-    BNE +checkIfFirstNonZeroCollision
-        ;STA tempA
-        LDA tempx
-        STA temp2
-        STY tempy
-        LDA #$01
-        RTS
-
-    +checkIfFirstNonZeroCollision:
-    STA temp
-    LDA tempy
-    BNE +checkNextPoint
-        LDA tempx
-        STA temp2
-        LDA temp
-        STA tempA
-        STY tempy
-    +checkNextPoint:
-ENDM
-
-
     ;; Reset the end result variable
     LDA #$00
     STA tempA
